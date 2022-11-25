c9e2503f240cf917fdc9bc6a27a3247a746b5fa2
wolf-null <wolfnull@yandex.ru>
2022-11-26 02:05:20 +0300

    fixed sorting mechanism

--- lib/dumb_templates/orderable_pair.cpp
+++ lib/dumb_templates/orderable_pair.cpp
@@ -1,30 +1,33 @@
 #include "pair.cpp"
 
+// --------------------------------------------------- DECLARATION -----------------------------------------------------
+
 template <typename T>
 concept Ordered = requires (T a, T b)
 {
     a < b;
 };
 
-
 template <Ordered TLeft, Ordered TRight>
 class OrderablePair : public Pair <TLeft, TRight>
 {
 public:
     OrderablePair(TLeft left, TRight right);
-
-    bool operator < (OrderablePair <TLeft, TRight>& other)
-    {
-        if(this->getLeft() < other.getLeft())
-            return true;
-
-        if(this->getRight() < other.getRight())
-            return true;
-
-        return false;
-    }
-
 };
+
+// ------------------------------------------------- IMPLEMENTATION ----------------------------------------------------
+
+template <Ordered TLeft, Ordered TRight>
+bool operator < (const OrderablePair <TLeft, TRight>& a, const OrderablePair <TLeft, TRight>& b)
+{
+    if(b.getLeft() < a.getLeft())
+        return false;
+
+    if(b.getRight() < a.getRight())
+        return false;
+
+    return true;
+}
 
 template<Ordered TLeft, Ordered TRight>
 OrderablePair<TLeft, TRight>::OrderablePair(TLeft left, TRight right) : Pair<TLeft, TRight>(left, right) {};
